decae21e6a65a6c9deb02db653c7b3f317f5ac75
Aanchal Adhikari <aadhikari37@gatech.edu>
2023-12-30 12:23:47 +0545

    Show battery info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,165 @@
-import Image from 'next/image'
+import axios from 'axios';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+interface GetParams {
+  pc_start?: number;
+  pc_end?: number;
+  gte_cap?: number;
+  lte_cap?: number;
+  search?: string;
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface ResponseData {
+  batteries: {
+    _id: string;
+    name: string;
+    capacity: number;
+    postalCode: number;
+    __v: number;
+    createdAt: string;
+    updatedAt: string;
+  }[];
+  totalWattCapacity: number;
+  averageWattCapacity: number;
 }
+
+export default async function Home() {
+  const response = await fetchData({
+    search: 'BB'
+  });
+
+  const powerHubData: ResponseData | undefined = response?.data; 
+
+  return (
+    <div>
+      <h1 className='block text-center font-bold text-lg p-4'>Battery Hub</h1>
+
+      {powerHubData && powerHubData.batteries.length > 0 && (
+      <div className="mb-8 px-40">
+        <table className="min-w-full border rounded-lg overflow-hidden">
+          <thead className="bg-gray-800 text-white">
+            <tr>
+              <th className="py-2 px-4 text-left">Name</th>
+              <th className="py-2 px-4 text-left">Postal Code</th>
+              <th className="py-2 px-4 text-left">Watt Capacity</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {powerHubData.batteries.map((battery) => (
+              <tr key={battery._id} className="bg-gray-100">
+                <td className="py-2 px-4 text-left">{battery.name}</td>
+                <td className="py-2 px-4 text-left">{battery.postalCode}</td>
+                <td className="py-2 px-4 text-left">{battery.capacity}W</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )}
+
+      <p></p>
+    </div>
+  );
+}
+
+const fetchData = async (params: GetParams) => {
+  try {
+    const response = await axios.get('http://localhost:5001/batteries',{params});
+    return response;
+  } catch (err) {
+
+  }
+
+};
+
+
+// "use client";
+
+// import { useState } from 'react';
+// import axios from 'axios';
+
+// export default function Home() {
+//   const [name, setName] = useState('');
+//   const [postcode, setPostcode] = useState('');
+//   const [wattCapacity, setWattCapacity] = useState('');
+
+//   const [startPostcode, setStartPostcode] = useState('');
+//   const [endPostcode, setEndPostcode] = useState('');
+//   const [filter, setFilter] = useState('');
+
+//   const [batteries, setBatteries] = useState([]);
+//   const [totalWattCapacity, setTotalWattCapacity] = useState(0);
+//   const [averageWattCapacity, setAverageWattCapacity] = useState(0);
+
+//   const addBattery = async () => {
+//     try {
+//       await axios.post('/api/batteries', {
+//         name,
+//         postcode,
+//         wattCapacity: parseInt(wattCapacity),
+//       });
+//       alert('Battery added successfully');
+//       setName('');
+//       setPostcode('');
+//       setWattCapacity('');
+//     } catch (error) {
+//       console.error(error);
+//       alert('Error adding battery');
+//     }
+//   };
+
+//   const searchBatteries = async () => {
+//     try {
+//       const response = await axios.get('/api/batteries', {
+//         params: { start: startPostcode, end: endPostcode, filter },
+//       });
+//       setBatteries(response.data.batteries);
+//       setTotalWattCapacity(response.data.totalWattCapacity);
+//       setAverageWattCapacity(response.data.averageWattCapacity);
+//     } catch (error) {
+//       console.error(error);
+//       alert('Error fetching batteries');
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Virtual Power Plant</h1>
+
+//       <div>
+//         <h2>Add Battery</h2>
+//         <label>Name:</label>
+//         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+//         <label>Postcode:</label>
+//         <input type="text" value={postcode} onChange={(e) => setPostcode(e.target.value)} />
+//         <label>Watt Capacity:</label>
+//         <input type="text" value={wattCapacity} onChange={(e) => setWattCapacity(e.target.value)} />
+//         <button onClick={addBattery}>Add Battery</button>
+//       </div>
+
+//       <div>
+//         <h2>Search Batteries</h2>
+//         <label>Start Postcode:</label>
+//         <input type="text" value={startPostcode} onChange={(e) => setStartPostcode(e.target.value)} />
+//         <label>End Postcode:</label>
+//         <input type="text" value={endPostcode} onChange={(e) => setEndPostcode(e.target.value)} />
+//         <label>Filter:</label>
+//         <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+//         <button onClick={searchBatteries}>Search Batteries</button>
+//       </div>
+
+//       <div>
+//         <h2>Battery List</h2>
+//         <p>Total Watt Capacity: {totalWattCapacity}</p>
+//         <p>Average Watt Capacity: {averageWattCapacity}</p>
+//         <ul>
+//           {batteries.map((battery) => (
+//             <li key={battery._id}>
+//               {battery.name} - {battery.postalCode} - {battery.capacity}W
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
